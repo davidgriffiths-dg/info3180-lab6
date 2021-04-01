31ae36298044bce97c253f24fdbdc6aae343cc46
David Griffiths <griffithsdavidchris@gmail.com>
2021-03-31 23:06:55 -0500

    page done use app.component

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -11,7 +11,7 @@ app.component('app-header', {
   name: 'AppHeader',
   template: `
       <header>
-          <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+          <nav class="navbar navbar-expand-lg navbar-dark bg-success fixed-top">
             <a class="navbar-brand" href="#">VueJS App</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
@@ -34,6 +34,72 @@ app.component('app-header', {
     return {};
   }
 });
+
+app.component('news-list', {
+  name:'NewsList',
+  template:`
+
+<div class="news">
+<h2>News</h2>
+<div class="form-inline d-flex justify-content-center">
+<div class="form-group mx-sm-3 mb-2">
+    <label class="sr-only" for="search">Search</label>
+    <input type="search" name="search" v-model="searchTerm"
+    id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+    <button class="btn btn-success mb-2"
+    @click="searchNews">Search</button>
+</div>
+</div>
+<ul class="news__list">
+<li v-for="article in articles"
+class="news__item"> <span class ="artitle">{{ article.title }}</span>
+<img :src=article.urlToImage> 
+{{article.description}}
+</li>
+</ul>
+</div>`,
+created() {
+  let self = this;
+  fetch('https://newsapi.org/v2/top-headlines?country=us',{
+  headers: {
+          'Authorization': 'Bearer '
+  }})
+  .then(function(response) {
+  return response.json();
+  })
+  .then(function(data) {
+  console.log(data);
+  self.articles = data.articles;
+  });
+  },
+  data() {
+    return {
+    articles: [],
+    searchTerm: ''
+    }
+    },
+    methods: {
+      searchNews() {
+      let self = this;
+      fetch('https://newsapi.org/v2/everything?q='+
+      self.searchTerm + '&language=en', {
+      headers: {
+      'Authorization': 'Bearer '
+      }
+      })
+      .then(function(response) {
+      return response.json();
+      })
+      .then(function(data) {
+      console.log(data);
+      self.articles = data.articles;
+      });
+      }
+      }
+  });
+
+
+
 
 app.component('app-footer', {
   name: 'AppFooter',
